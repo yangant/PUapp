f765d123a1fa2df4be3df5decfc163eae6564536
Sunny <583954049@qq.com>
2018-04-10 16:08:13 +0800

    code

--- app/src/main/java/com/example/administrator/powerup/BlankFragmentThree.java
+++ app/src/main/java/com/example/administrator/powerup/BlankFragmentThree.java
@@ -5,9 +5,11 @@ package com.example.administrator.powerup;
  */
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +21,19 @@ import android.widget.EditText;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class BlankFragmentThree extends Fragment {
+
+    private static String url = "http://192.168.191.3:8080/ServletTestOne/"; // IP地址请改为你自己的IP
+
+    private static String URL_Register = url + "FriendsServlet";
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -124,8 +138,72 @@ public class BlankFragmentThree extends Fragment {
                         .setView(edt)
                         .setPositiveButton("确定", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface arg0, int arg1) {
-                                Toast t = Toast.makeText(getContext(),"任务已满，请先完成任务！", Toast.LENGTH_LONG);
-                                t.show();
+                                if (edt.getText().toString().equals(LogInActivity.myPlayer.getPlayer_name())) {
+                                    Toast t = Toast.makeText(getContext(),"请勿输入自己的名称！", Toast.LENGTH_LONG);
+                                    t.show();
+                                } else {
+                                    new AsyncTask<String , Integer, String>(
+                                    ){
+                                        @Override
+                                        protected void onPreExecute() {
+                                            Log.w("WangJ", "task onPreExecute()");
+                                        }
+
+                                        /**
+                                         * @param params 这里的params是一个数组，即AsyncTask在激活运行是调用execute()方法传入的参数
+                                         */
+                                        @Override
+                                        protected String doInBackground(String... params) {
+                                            Log.w("WangJ", "task doInBackground()");
+                                            HttpURLConnection connection = null;
+                                            StringBuilder response = new StringBuilder();
+                                            try {
+                                                String registerUrlStr = URL_Register + "?operation=1" + "&playerID=" + LogInActivity.myPlayer.getPlayer_name() + "&friendID=" + edt.getText().toString();
+                                                URL url = new URL(registerUrlStr); // 声明一个URL,注意如果用百度首页实验，请使用https开头，否则获取不到返回报文
+                                                connection = (HttpURLConnection) url.openConnection(); // 打开该URL连接
+                                                connection.setRequestMethod("GET"); // 设置请求方法，“POST或GET”，我们这里用GET，在说到POST的时候再用POST
+                                                connection.setConnectTimeout(80000); // 设置连接建立的超时时间
+                                                connection.setReadTimeout(80000); // 设置网络报文收发超时时间
+                                                InputStream in = connection.getInputStream();  // 通过连接的输入流获取下发报文，然后就是Java的流处理
+                                                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                                                String line;
+                                                while ((line = reader.readLine()) != null) {
+                                                    response.append(line);
+                                                }
+                                            } catch (MalformedURLException e) {
+                                                e.printStackTrace();
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                            return response.toString(); // 这里返回的结果就作为onPostExecute方法的入参
+                                        }
+
+                                        @Override
+                                        protected void onProgressUpdate(Integer... values) {
+                                            // 如果在doInBackground方法，那么就会立刻执行本方法
+                                            // 本方法在UI线程中执行，可以更新UI元素，典型的就是更新进度条进度，一般是在下载时候使用
+                                        }
+
+                                        /**
+                                         * 运行在UI线程中，所以可以直接操作UI元素
+                                         * @param s
+                                         */
+                                        @Override
+                                        protected void onPostExecute(String s) {
+                                            if (s.equals("100")) {
+                                                Toast t = Toast.makeText(getContext(),"添加好友成功！", Toast.LENGTH_LONG);
+                                                t.show();
+                                            } else if (s.equals("300")) {
+                                                Toast t = Toast.makeText(getContext(),"此用户不存在！", Toast.LENGTH_LONG);
+                                                t.show();
+                                            } else if (s.equals("200")) {
+                                                Toast t = Toast.makeText(getContext(),"添加好友失败！", Toast.LENGTH_LONG);
+                                                t.show();
+                                            }
+                                        }
+
+                                    }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                                }
                             }
                         })
                         .setNegativeButton("取消", null)
--- app/src/main/java/com/example/administrator/powerup/TaskActivity.java
+++ app/src/main/java/com/example/administrator/powerup/TaskActivity.java
@@ -273,69 +273,10 @@ public class TaskActivity extends AppCompatActivity {
     }
 
     public void finishTask(View view) {
-        new AsyncTask<String , Integer, String>(
-        ){
-            @Override
-            protected void onPreExecute() {
-                Log.w("WangJ", "task onPreExecute()");
-            }
-
-            /**
-             * @param params 这里的params是一个数组，即AsyncTask在激活运行是调用execute()方法传入的参数
-             */
-            @Override
-            protected String doInBackground(String... params) {
-                Log.w("WangJ", "task doInBackground()");
-                HttpURLConnection connection = null;
-                StringBuilder response = new StringBuilder();
-                try {
-                    String registerUrlStr = URL_Register + "?operation=3" + "&taskname=" + LogInActivity.myPlayer.getPlayer_tasks().get(po).getTask_name() + "&content=" + LogInActivity.myPlayer.getPlayer_tasks().get(po).getTask_content() + "&style=" + LogInActivity.myPlayer.getPlayer_tasks().get(po).getTask_style() + "&duration=" + LogInActivity.myPlayer.getPlayer_tasks().get(po).getTask_duration() + "&playerID=" + LogInActivity.myPlayer.getPlayer_name();
-                    URL url = new URL(registerUrlStr); // 声明一个URL,注意如果用百度首页实验，请使用https开头，否则获取不到返回报文
-                    connection = (HttpURLConnection) url.openConnection(); // 打开该URL连接
-                    connection.setRequestMethod("GET"); // 设置请求方法，“POST或GET”，我们这里用GET，在说到POST的时候再用POST
-                    connection.setConnectTimeout(80000); // 设置连接建立的超时时间
-                    connection.setReadTimeout(80000); // 设置网络报文收发超时时间
-                    InputStream in = connection.getInputStream();  // 通过连接的输入流获取下发报文，然后就是Java的流处理
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
-                    }
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                return response.toString(); // 这里返回的结果就作为onPostExecute方法的入参
-            }
-
-            @Override
-            protected void onProgressUpdate(Integer... values) {
-                // 如果在doInBackground方法，那么就会立刻执行本方法
-                // 本方法在UI线程中执行，可以更新UI元素，典型的就是更新进度条进度，一般是在下载时候使用
-            }
-
-            /**
-             * 运行在UI线程中，所以可以直接操作UI元素
-             * @param s
-             */
-            @Override
-            protected void onPostExecute(String s) {
-                if (s.equals("200")) {
-                    Toast t = Toast.makeText(TaskActivity.this,"完成任务成功", Toast.LENGTH_LONG);
-                    t.show();
-                    LogInActivity.myPlayer.getPlayer_tasks().remove(po);
-                    Intent intent = new Intent(TaskActivity.this, MainActivity.class);
-                    startActivity(intent);
-                } else if (s.equals("300")) {
-                    Toast t = Toast.makeText(TaskActivity.this,"完成任务失败", Toast.LENGTH_LONG);
-                    t.show();
-                }
-            }
-
-        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         Intent intent = new Intent(TaskActivity.this, FinishingTaskActivity.class);
         intent.putExtra("position", po);
+        intent.putExtra("taskname", newTask.getTask_name());
+        intent.putExtra("content", newTask.getTask_content());
         intent.putExtra("style", newTask.getTask_style());
         intent.putExtra("duration", newTask.getTask_duration());
         startActivity(intent);
